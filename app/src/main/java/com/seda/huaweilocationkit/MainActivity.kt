ee651aed6b2223d6391febba14446644336b186e
Seda Nur Önder <sednrondr@gmail.com>
2023-01-02 22:12:39 +0300

    huawei location add

--- app/src/main/java/com/seda/huaweilocationkit/MainActivity.kt
+++ app/src/main/java/com/seda/huaweilocationkit/MainActivity.kt
@@ -1,11 +1,88 @@
 package com.seda.huaweilocationkit
 
+import android.Manifest
+import android.content.ContentValues.TAG
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // Dynamically apply for required permissions if the API level is 28 or smaller.
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            Log.i(TAG, "android sdk <= 28 Q")
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                val strings = arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                ActivityCompat.requestPermissions(this, strings, 1)
+            }
+        } else {
+            // Dynamically apply for required permissions if the API level is greater than 28. The android.permission.ACCESS_BACKGROUND_LOCATION permission is required.
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    "android.permission.ACCESS_BACKGROUND_LOCATION"
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                val strings = arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    "android.permission.ACCESS_BACKGROUND_LOCATION"
+                )
+                ActivityCompat.requestPermissions(this, strings, 2)
+            }
+        }
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        var settingsClient = LocationServices.getSettingsClient(this)
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.interval = 10000
+// Set the location type.
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+
+        val mLocationCallback: LocationCallback
+        mLocationCallback = object : LocationCallback() {
+
+
+            override fun onLocationResult(locationResult: LocationResult) {
+                if (locationResult != null) {
+                    // TODO: Process the location callback result.
+                }
+            }
+        }
+
+        fusedLocationProviderClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.getMainLooper())
+            .addOnSuccessListener {
+                // TODO: Define callback for API call success.
+            }
+            .addOnFailureListener {
+                // TODO: Define callback for API call failure.
+            }
     }
 }
